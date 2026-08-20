3d173e5964f172b0b7f7e073775452f5c8aacb37
SANJO777 <santiago11200311@gmail.com>
2026-08-20 15:43:11 -0500

    Creé la interfaz informal (el qué es), para más adelante crear los métodos y atributos (el cómo). Asimismo, descubrí que necesito otra clase para el menú porque tiene muchas más complejidad y es mejor dividir por partes el problema hasta hacerlo pequeño para resolverlo (eso creo).

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print("¡Hola, Mundo!")
+from decimal import *
+
+class CalculadoraSimple:
+
+    def __init__(self, nombre_calculadora):
+        self.nombre_calculadora = nombre_calculadora
+
+    # Menú en consola. Asimismo, retorna al menú principal al imprimir el resultado de la operación hecha.
+    def menú(self):
+        pass
+
+    # Valida números racionales, operadores y expresiones simples ingresados para operar, en vez de letras o carácteres inválidos.
+    def validar_números(self):
+        pass
+
+    # Operación Aritmética: Adición.
+    def sumar(self):
+        pass
+
+    # Operación Aritmética: Substracción.
+    def resta(self):
+        pass
+
+    # Operación Aritmética: Multiplicación.
+    def multiplicación(self):
+        pass
+
+    # Operación Aritmética: División.
+    def división(self):
+        pass
+
+    # Operación Aritmética: Porcentaje.
+    def porcentaje(self):
+        pass
+
+calculadora_simple = CalculadoraSimple("Kuromi")
+print(calculadora_simple.nombre_calculadora)
